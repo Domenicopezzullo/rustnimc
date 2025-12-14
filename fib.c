d4418e38cd3f49f995e75c4859289b96ea4419e9
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-12-14 13:32:56 +0100

    c format

--- fib.c
+++ fib.c
@@ -1,39 +1,39 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 
 static inline int32_t fib(int32_t n, int32_t *memo) {
-    if (n < 2) {
-        return n;
-    }
-    if (memo[n] != -1) {
-        return memo[n];
-    }
-    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  if (n < 2) {
+    return n;
+  }
+  if (memo[n] != -1) {
     return memo[n];
+  }
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
 }
 
 int main(void) {
-    const int32_t n = 10;
-    int32_t *memo = (int32_t *)malloc((n + 1) * sizeof(int32_t));
+  const int32_t n = 10;
+  int32_t *memo = (int32_t *)malloc((n + 1) * sizeof(int32_t));
 
-    if (memo == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-        return 1;
-    }
+  if (memo == NULL) {
+    fprintf(stderr, "Memory allocation failed\n");
+    return 1;
+  }
 
-    for (int32_t i = 0; i <= n; i++) {
-        memo[i] = -1;
-    }
+  for (int32_t i = 0; i <= n; i++) {
+    memo[i] = -1;
+  }
 
-    int32_t acc = 0;
-    for (int32_t i = 0; i < 1000000; i++) {
-        acc += fib(n, memo);
-    }
+  int32_t acc = 0;
+  for (int32_t i = 0; i < 1000000; i++) {
+    acc += fib(n, memo);
+  }
 
-    printf("%d\n", acc);
+  printf("%d\n", acc);
 
-    free(memo);
-    return 0;
+  free(memo);
+  return 0;
 }
