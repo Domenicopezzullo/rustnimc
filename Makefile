58fc33d3e7466f8a2c8a3b2a372f99f6e620cb71
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-12-14 15:16:36 +0100

    Hello

--- Makefile
+++ Makefile
@@ -8,6 +8,6 @@ mkdir:
 out/fib_rs: fib.rs | mkdir
 	rustc -C opt-level=3 -C lto="fat" -C codegen-units=1 fib.rs -o out/fib_rs
 out/fib_nim: fib.nim | mkdir
-	nim c --debuginfo -o:out/fib_nim --mm:refc  -d:danger --checks=off --opt=speed --passC:"-funroll-loops -ffast-math -O3 -flto -mtune=native -march=native" --passL:"-flto" fib.nim
+	nim c --nimcache:./output --debuginfo -o:out/fib_nim --mm:refc  -d:danger --checks=off --opt=speed --passC:"-funroll-loops -ffast-math -O3 -flto -mtune=native -march=native" --passL:"-flto" fib.nim
 out/fib_c: fib.c | mkdir
 	cc -o out/fib_c fib.c -O3 -flto -ffast-math -march=native
--- fib.nim
+++ fib.nim
@@ -1,3 +1,5 @@
+import std/[os, strutils]
+
 proc fib(n: int32, memo: var seq[int32]): int32 {.inline.} =
   if n < 2:
     return n
@@ -6,12 +8,16 @@ proc fib(n: int32, memo: var seq[int32]): int32 {.inline.} =
   memo[n] = fib(n-1, memo) + fib(n-2,memo)
   return memo[n]
 
-let n: int32 = 10
+let cmdline = os.commandLineParams()
+if cmdline.len < 1:
+  echo "Usage: fib_nim <n>"
+  quit 1
+let n = parseInt(cmdline[0]).int32
 var memo = newSeq[int32](n+1)
-for n in mitems memo:
-  n = -1
+for i in mitems memo:
+  i = -1
 
 var acc: int32 = 0
-for _ in 0..1_000_000:
-  acc += fib(int32(n), memo)
+for _ in 1..1_000_000:
+  acc = acc xor fib(int32(n), memo)
 echo acc
